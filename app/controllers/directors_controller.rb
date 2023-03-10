8f4f0e0d475cdadbefb4c9c6326f8cb6534f5138
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-03-10 22:57:03 +0000

    MSM Queries

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,23 +1,37 @@
 class DirectorsController < ApplicationController
 
   def index
-    @ list_of_directors = Director.all
+    @list_of_directors = Director.all
 
     render({ :template => "director_templates/index.html.erb" })
   end
 
+  def show_movies
+    @list_of_movies = Movie.all
+    @list_of_directors = Director.all
+
+    render({ :template =>"misc_templates/play.html.erb "})
+  end
+
   def wisest
-    @oldest = Director.where.not ({ :dob => nil}).order({:dob =>:asc }).at(0)
+    @oldest = Director.where.not({:dob => nil}).order({:dob =>:asc }).at(0)
 
     render({ :template => "director_templates/eldest.html.erb" })
+
+  end
+  def youngest
+
+    @youngest = Director.where.not({:dob => nil}).order({:dob =>:asc }).at(0)
+
+    render({ :template => "director_templates/youngest.html.erb" })
 
   end
 
   def director_details
 
-    the_id = params.fetch("an_id")
+    @the_id = params.fetch("an_id")
 
-    @the_director = Director.where({ :id => the_id }).at(0)
+    @the_director = Director.where({ :id => @the_id }).at(0)
     @filmography = Movie.where({ :director_id => 2})
 
     render({ :template => "director_templates/show.html.erb" })
---
+++ app/controllers/school_controller.rb
@@ -0,0 +1,7 @@
+class SchoolController < ApplicationController
+
+def play 
+
+  render({ :template => "movies_templates/play.html.erb" })
+end
+end
